317c0962143dc9e84f61499e19d492749b3ac5c0
Pincodeert <leonie.pin@gmail.com>
2021-09-08 16:21:16 +0200

    aangepaste regel

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("AAAAAAAAAAHHHHHH");
         System.out.println("oje");
-
+        System.out.println("valt toch mee");
     }
 }
