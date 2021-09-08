2c7b8ac569d8c3b9b532176582213c4314c2748f
Pincodeert <leonie.pin@gmail.com>
2021-09-08 16:19:39 +0200

    eerste commit

---
+++ src/novi/basics/Main.java
@@ -0,0 +1,10 @@
+package novi.basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAHHHHHH");
+        System.out.println("oje");
+
+    }
+}
